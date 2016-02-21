a670976f965353e7f443d7bf8194416a34da6447
Shujian Yang <yang_shujian@hotmail.com>
2016-02-21 13:58:33 -0600

    Improve LIS

--- LIS/lis.cpp
+++ LIS/lis.cpp
@@ -1,19 +1,76 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int lengthOfLIS(vector<int>& nums);
+int lengthOfLIS2(vector<int>& nums);
 
-int main()
+int main(int argc, char *argv[])
 {
-	vector<int> list = {10, 9, 2, 5, 3, 7, 101, 18};
+    srand(time(0));
+    
+	vector<int> list;
+    
+    int count;
+    cout << "Enter number of integers:";
+    cin >> count;
+    
+    for(int i=0; i<count; ++i){
+        list.push_back(rand() % 1000);
+    }
+    
+    //for(auto& val : list)
+    //    cout << val << " ";
+    //cout << endl;
+    clock_t start, end;
+    int clock_per_ms = CLOCKS_PER_SEC / 1000;
+    
+    start = clock();
+	cout << lengthOfLIS2(list) << endl;
+    end = clock();
+    cout << "Time:" << double(end - start) / clock_per_ms << "ms" << endl;
+    
+    
+    start = clock();
 	cout << lengthOfLIS(list) << endl;
+    end = clock();
+    cout << "Time:" << double(end - start) / clock_per_ms << "ms" << endl;
 
 	return 0;
 }
 
 int lengthOfLIS(vector<int>& nums)
+{
+	int len = nums.size();
+	vector<int> lis;
+	
+	for(auto& val : nums){
+        int min = 0;
+        int max = lis.size() - 1;
+        while(min <= max){
+            int mid = (min + max) / 2;
+            if(lis[mid] == val){
+                min = mid;
+                break;
+            }
+            else if(lis[mid] > val)
+                max = mid - 1;
+            else
+                min = mid + 1;
+        }
+        if(min < lis.size())
+            lis[min] = val;
+        else
+            lis.push_back(val);
+    }
+    
+    return lis.size();
+}
+
+int lengthOfLIS2(vector<int>& nums)
 {
 	int len = nums.size();
 	vector<int> lis(len, 1);
